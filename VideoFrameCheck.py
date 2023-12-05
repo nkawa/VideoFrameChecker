269239f452c0c83ea67fc8a8fe4becc264428124
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2023-12-05 18:56:38 +0900

    update for CSV

--- VideoFrameCheck.py
+++ VideoFrameCheck.py
@@ -4,6 +4,51 @@ from tkinter import *
 from PIL import Image, ImageTk, ImageOps  # 画像データ用
 import sys
 import tkinter as tk
+from tkinter import filedialog
+import glob
+import pandas as pd
+from pathlib import Path
+
+
+def ts2sec(tstr):
+    h = int(tstr[:-6])
+    m = int(tstr[-5:-3])
+    s = int(tstr[-2:])
+    return h*3600+m*60+s
+def sec2ts(sec):
+    s = sec%60
+    m = int(sec/60)%60
+    h = int(sec/3600)
+    return "{:02d}{:02d}{:02d}".format(h,m,s)
+
+def sec2ts2(sec):
+    s = sec%60
+    m = int(sec/60)%60
+    h = int(sec/3600)
+    return "{:02d}:{:02d}:{:02d}".format(h,m,s)
+
+# 時刻の先頭に 0　を追加するだけ。
+def add_recog_0(x):
+    return ("0"+x)[-8:]
+
+def read_timestamp(fname):
+    df = pd.read_csv(fname, usecols=[0,1,2,3,4])
+    df['recog']=df['recog'].map(add_recog_0)
+    df['sec']= df['recog'].map(ts2sec)
+    return df
+
+def check_timestamp(df):
+    ldiff = df['sec'][0]
+    error_index = []
+    for i,row in df.iterrows():
+        diff = row['sec']
+        if ldiff +1 != diff and ldiff !=diff:
+            print(i,"vid",row['vid_idx'],"frm",row['frm_idx'],lstr,ldiff,row['recog'],diff)
+            error_index.append(i)
+        lstr = row['recog']
+        ldiff = diff
+
+    return error_index
 
 class App(tk.Frame):
     def __init__(self,master = None):
@@ -16,6 +61,15 @@ class App(tk.Frame):
         self.canvas = tk.Canvas(self.image_frame, width = 960, height = 540)
 #        self.canvas.bind('<Button-1>', self.canvas_click)
         self.canvas.pack(expand = True, fill = tk.BOTH, anchor=tk.CENTER, padx=10)
+
+        self.csv_file = tk.Label(self.button_frame, text = "CSV: Not set")
+        self.csv_file.pack(expand = True, padx=10, pady=10)
+
+        self.csv_button = tk.Button(self.button_frame, text="Open CSV", command=self.openCSV, width=40)
+        self.csv_button.pack(expand = True, fill = tk.X, padx=10, pady=10)
+
+        self.frame_num = tk.Label(self.button_frame,text="<-Frame:_")
+        self.frame_num.pack(expand=True, fill= tk.X, padx = 10, pady = 10)
 
         self.num = tk.Entry(self.button_frame,width=10)
         self.num.insert(tk.END,"0")  
@@ -37,6 +91,15 @@ class App(tk.Frame):
 #        else:
  #           self.disp_id = None
 
+    def openCSV(self):
+        path = filedialog.askopenfilename(defaultextension=".csv",filetypes=[("CSV","*.csv")],title="Open csv file")
+        print(path)
+        self.csv_file["text"]="CSV:"+path
+        if len(path)>0:
+            df = read_timestamp(path)
+            check_timestamp(df)
+
+
     def next(self):
         frame = int(self.num.get())
 #        cap.
@@ -44,6 +107,11 @@ class App(tk.Frame):
         if app.frame != frame:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES,frame)
             app.frame = frame
+
+        # 現在のフレーム番号
+        rframe = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
+        self.frame_num["text"] = "<----  Frame: "+str(rframe)+", try to set: "+str(frame)
+
         tf, img = self.cap.read()
         # 画像を半分のサイズにしたい
         app.frame += 1
@@ -66,7 +134,7 @@ class App(tk.Frame):
                 270,                   
                 image=self.pimg  # 表示画像データ
                 )
-        print("Show image",tf)        
+#        print("Show image",tf)        
 
     
 if __name__ == "__main__":
